496178d60c2cc6523e247944ec2233aad96e9968
joep-source <55184540+joep-source@users.noreply.github.com>
2022-01-16 14:55:13 +0100

    improved world mapping

--- play.py
+++ play.py
@@ -1,10 +1,20 @@
+from typing import List
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
 from generate_world import world_init
+from utils import pos_to_xyz
 
 
 class CustomFirstPersonController(FirstPersonController):
+    position_previous = None
+
+    def __init__(self, position_start, **kwargs):
+        super().__init__()
+        self.position = self.position_previous = position_start
+        self.update()
+        print(f"Position start {self.position}")
+
     def input(self, key):
         if key == "space":
             self.gravity = 1
@@ -17,8 +27,14 @@ class CustomFirstPersonController(FirstPersonController):
         if key == "escape":
             quit()
 
-    def pos(self, axis):
-        return floor(getattr(self.position, axis))
+    def new_position(self) -> bool:
+        pos_cur = pos_to_xyz(self.position)
+        pos_old = pos_to_xyz(self.position_previous)
+        if (pos_cur[0], pos_cur[2]) != (pos_old[0], pos_old[2]):
+            print(f"Position new {pos_to_xyz(self.position)}")
+            self.position_previous = self.position
+            return True
+        return False
 
 
 class Block(Button):
@@ -34,9 +50,8 @@ class Block(Button):
 
         super().__init__(
             parent=scene,
-            position=position,
+            position=[pos + 0.5 for pos in position],
             model="cube",
-            origin_y=0.5,
             texture="white_cube",
             color=colour,
             highlight_color=color.lime,
@@ -51,84 +66,72 @@ class Block(Button):
 
 
 class UrsinaMC(Ursina):
-    blocks = []
+    blocks: List[Block] = []
     render_size = 16
     world_size = 100
 
     def __init__(self, **kwargs):
         self.world_map = world_init(self.world_size)
-        self.player_position_start = [50, 3, 50]
-        self.world_create()
+        position_start = [15.5, 1, 15.5]
+        self.world_create(position_start=position_start)
         super().__init__(**kwargs)
-        self.gravity = 0
-        self.player = CustomFirstPersonController()
-        self.player_old_pos = self.player.position
+        self.player = CustomFirstPersonController(position_start=position_start)
 
     def _update(self, task):
-        if self.new_pos():
-            print(f"blocks {len(self.blocks)}")
+        if self.player.new_position():
+            # print(f"Total blocks {len(self.blocks)}")
             self.world_move_destroy()
             self.world_move_create()
         self.world_click_handler()
         return super()._update(task)
 
-    def new_pos(self):
-        for i in range(3):
-            if floor(self.player.position[i]) != floor(self.player_old_pos[i]):
-                self.player_old_pos = self.player.position
-                return True
-        return False
-
     # ========================================================================
 
-    def pos_player_to_world(self, x, z):
-        new_x = int(x) + self.player_position_start[0]
-        new_z = int(z) + self.player_position_start[2]
-        return [new_x, new_z]
+    def world_render_block(self, position):
+        x, y, z = pos_to_xyz(position)
+        if all([0 <= pos < self.world_size for pos in (x, z)]):
+            colour = self.world_map[x][z].color(multiply=255)
+            self.blocks.append(Block(position=(x, y, z), colour=colour))
 
-    def world_render_block(self, x, z):
-        world_pos = self.pos_player_to_world(x=x, z=z)
-        if all([0 <= pos < self.world_size for pos in world_pos]):
-            colour = self.world_map[world_pos[0]][world_pos[1]].color(multiply=255)
-            self.blocks.append(Block(position=(x, 0, z), colour=colour))
-
-    def world_create(self):
-        for z in range(-self.render_size, self.render_size):
-            for x in range(-self.render_size, self.render_size):
-                self.world_render_block(x=x, z=z)
+    def world_create(self, position_start=[0, 0, 0]):
+        for z in range(-self.render_size, self.render_size + 1):
+            z += position_start[2]
+            for x in range(-self.render_size, self.render_size + 1):
+                x += position_start[0]
+                self.world_render_block([x, 0, z])
 
     def world_move_destroy(self):
-        __blocks_len = len(self.blocks)
-        player_x, player_z = self.player.pos("x"), self.player.pos("z")
-        block: Block
+        # __blocks_len = len(self.blocks)
+        player_x, _, player_z = pos_to_xyz(self.player.position)
         for block in reversed(self.blocks):
-            if self.render_size < max(
-                abs(block.position.x - player_x), abs(block.position.z - player_z)
+            if (
+                block.position.x < player_x - self.render_size + 0.5
+                or block.position.x > player_x + self.render_size + 0.5
+                or block.position.z < player_z - self.render_size + 0.5
+                or block.position.z > player_z + self.render_size + 0.5
             ):
                 self.blocks.remove(block)
                 destroy(block)
-        print(f"del {__blocks_len - len(self.blocks)} blocks")
+        # print(f"del {__blocks_len - len(self.blocks)} blocks")
 
     def world_move_create(self):
-        __blocks_len = len(self.blocks)
-        blocks_x = set([b.position.x for b in self.blocks])
-        blocks_z = set([b.position.z for b in self.blocks])
+        # __blocks_len = len(self.blocks)
+        blocks_x = set([int(b.position.x) for b in self.blocks])
+        blocks_z = set([int(b.position.z) for b in self.blocks])
 
-        pos_x = self.player.pos("x")
-        pos_z = self.player.pos("z")
+        player_x, _, player_z = pos_to_xyz(self.player.position)
         size = self.render_size
-
-        new_x = [n for n in [pos_x - size, pos_x + size] if n not in blocks_x]
-        new_z = [n for n in [pos_z - size, pos_z + size] if n not in blocks_z]
+        new_x = [n for n in [player_x - size, player_x + size] if n not in blocks_x]
+        new_z = [n for n in [player_z - size, player_z + size] if n not in blocks_z]
 
         for x in new_x:
             for z in blocks_z:
-                self.world_render_block(x=x, z=z)
+                self.world_render_block([x, 0, z])
             blocks_x.add(x)
         for z in new_z:
             for x in blocks_x:
-                self.world_render_block(x=x, z=z)
-        print(f"add {len(self.blocks) - __blocks_len} blocks")
+                self.world_render_block([x, 0, z])
+        # print(f"add {len(self.blocks) - __blocks_len} blocks")
 
     def world_click_handler(self):
         for block in reversed(self.blocks):
---
+++ utils.py
@@ -0,0 +1,2 @@
+def pos_to_xyz(position):
+    return int(position[0]), int(position[1]), int(position[2])
